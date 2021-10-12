a09991f1cf23a909f24bfe2f05a6d4ee03a8241e
Pitkincorp <pitkincorp@gmail.com>
2021-10-12 16:04:26 +0300

    New problem Stepik API JSON requests

---
+++ St_11(api_request).py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+client_id = 'a852b6d647199299ec81'
+client_secret = '889f62bdf9cc5c3e9c2780ead45aeef3'
+
+# инициируем запрос на получение токена
+r = requests.post("https://api.artsy.net/api/tokens/xapp_token",
+                  data={
+                      "client_id": client_id,
+                      "client_secret": client_secret
+                  })
+
+# разбираем ответ сервера
+j = json.loads(r.text)
+
+# достаем токен
+token = j["token"]
+
+# создаем заголовок, содержащий наш токен
+headers = {"X-Xapp-Token" : token}
+
+# инициируем запрос с заголовком
+r = requests.get("https://api.artsy.net/api/artists/4d8b92b34eb68a1b2c0003f4", headers=headers)
+
+# разбираем ответ сервера
+j = json.loads(r.text)
