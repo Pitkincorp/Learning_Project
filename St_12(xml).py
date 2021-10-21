c2282156b0d200500d3c2e9255f847eb237d70d2
Pitkincorp <pitkincorp@gmail.com>
2021-10-21 16:30:03 +0300

    St_12 xml 4 edition

--- St_12(xml).py
+++ St_12(xml).py
@@ -7,6 +7,12 @@ def lev_count(root):
     level = 0
     ans = {"red": 0, "green": 0, "blue": 0}
 
+    def lev_recursion(root):
+        nonlocal level
+        nonlocal ans
+        for element in root.ET.findall("cube"):
+            pass
+
     return f"{ans['red']} {ans['green']} {ans['blue']}"
 
 
