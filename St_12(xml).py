41e114809f24c1210d2791cd629edf4f9ba9c49e
Pitkincorp <pitkincorp@gmail.com>
2021-10-21 15:33:11 +0300

    St_12 xml third edition

--- St_12(xml).py
+++ St_12(xml).py
@@ -1,12 +1,13 @@
 import xml.etree.ElementTree as ET
 
-root = ET.fromstring(input())
-
-level = 0
+root = ET.fromstring(input())  # create root-object
 
 
-def lev_count():
-    pass
+def lev_count(root):
+    level = 0
+    ans = {"red": 0, "green": 0, "blue": 0}
+
+    return f"{ans['red']} {ans['green']} {ans['blue']}"
 
 
-print(root.tag, root.attrib)
+print(lev_count(root))
