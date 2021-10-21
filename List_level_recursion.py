3cd3c101f40a63f16724aaba448e72932e13b347
Pitkin <pitkicorp@gmail.com>
2021-10-21 20:11:29 +0300

    List_level_recursion final version

--- List_level_recursion.py
+++ List_level_recursion.py
@@ -13,7 +13,45 @@ ls = [
             ],
         0,
         0,
-        0
+        0,
+        [
+            0,
+            0
+        ]
+        ],
+    [
+        [
+            [
+                [
+                    0
+                ]
+            ]
         ]
     ]
-print(ls)
+    ]
+
+def lev_rec(ls, el):
+    '''accepts iterable sequence and returns the quantity of
+     elements on each level of matrix'''
+
+    def recursion(ls, el, level):
+        nonlocal ans
+        level += 1
+        # number = 0
+        for i in ls:
+            if i == el:
+                # number += 1
+                if level in ans:
+                    ans[level] += 1
+                else:
+                    ans[level] = 1
+            elif type(i) == list:
+                recursion(i, el, level)
+
+
+    level = 0
+    ans = {}
+    recursion(ls, el, level)
+    return ans
+
+print(lev_rec(ls, 0))
