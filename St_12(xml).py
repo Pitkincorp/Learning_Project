1847d434db1e118a9b5b4c38e7fe2c7dfbc44925
Pitkincorp <pitkincorp@gmail.com>
2021-10-22 11:06:16 +0300

    St_12 xml recursion final

--- St_12(xml).py
+++ St_12(xml).py
@@ -4,14 +4,17 @@ root = ET.fromstring(input())  # create root-object
 
 
 def lev_count(root):
-    level = 0
-    ans = {"red": 0, "green": 0, "blue": 0}
 
-    def lev_recursion(root):
-        nonlocal level
-        nonlocal ans
-        for element in root.ET.findall("cube"):
-            pass
+    def lev_recursion(root, level):
+        level += 1
+        for element in root.findall("cube"):
+            ans[element.attrib['color']] += level
+            lev_recursion(element, level)
+
+    level = 1
+    ans = {"red": 0, "green": 0, "blue": 0}
+    ans[root.attrib['color']] += level
+    lev_recursion(root, level)
 
     return f"{ans['red']} {ans['green']} {ans['blue']}"
 
