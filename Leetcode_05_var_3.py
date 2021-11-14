e58632678b901cb6b16246dc4d589cba30fce9f8
Pitkin <pitkicorp@gmail.com>
2021-11-14 11:53:35 +0300

    Leetcode_05 var 3 final (dictionary of symmetry centers and palindroms lengths

--- Leetcode_05_var_3.py
+++ Leetcode_05_var_3.py
@@ -4,26 +4,27 @@ def longestPalindrome(s):
     if len(set(s)) == 1:
         return s
 
-    centers = {}  # index of centers of palindroms and their length
+    centers = {}  # index of centers of palindroms and their lengths
     ans = s[0]  # first symbol is our initial answer
     for i in range(1, lth):
         for center,length in centers.items():
             if length > 0:
-                if s[i] == s[i-length-1]:
+                if (i - length - 1)>= 0 and s[i] == s[i-length-1]:
                     centers[center] += 2
                 else:
                     if length > len(ans):
                         ans = s[i-length:i]
                     centers[center] = 0
-        centers[i] = 1  # every new symbol is a center of minimal palindrom with length=1
+
         if s[i] == s[i - 1]:  # every doubled symbols have center of symmetry between them
-            centers[(i - 1 + i)/2] = 2
-    if len(ans) >= max(centers.values()):
+            centers[i - 0.5] = 2
+        if (i-2)>=0 and s[i] == s[i-2]:
+                    centers[i - 1] = 3
+    if not centers or len(ans) >= max(centers.values()):
         return ans
-    print(f"{ans=}")
     maxim = max(centers.values())
     return s[lth-maxim:]
 
 
-s = "ardrar"
+s = "par"
 print(f"lP= {longestPalindrome(s)}")
