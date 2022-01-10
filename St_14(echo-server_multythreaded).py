d5155157919479b8aca1084ef00ffc0405739757
Pitkin <pitkicorp@gmail.com>
2022-01-10 20:26:04 +0300

    server threading

--- St_14(echo-server_multythreaded).py
+++ St_14(echo-server_multythreaded).py
@@ -2,18 +2,27 @@ import socket, threading
 
 
 def server(conn, addr):
-    data = conn.recv(1024)
-    if not data or data == 'close':
-        conn.close()
-    conn.send(data)
+    while True:
+        data = conn.recv(1024)
+        if data == 'close' or not data:
+            break
+        conn.send(data)
     conn.close()
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('0.0.0.0', 2222))
-sock.listen(10)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('0.0.0.0', 2222))
+s.listen(10)
 while True:
-    conn, addr = sock.accept()
-    print("Connection", addr)
-    t = threading.Thread(target=server, args=(conn,addr))
+    conn, addr = s.accept()
+    t = threading.Thread(target=server, args=(conn, addr))
     t.start()
+
+"""Можно сделать через github
+В терминале:
+
+> git init
+
+> git remote add origin <git_url.git> https://github.com/Pitkincorp/Learning_Project/blob/c63e1e5c7fd65cc9f78d2f9ac89c2d3d399f5cac/St_14(echo-server_multythreaded).py
+
+> git pull origin master """
